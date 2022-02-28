b5a1378492f3e786c86d6a92e3f4727b2640b603
RoseQSun <70708796+RoseQSun@users.noreply.github.com>
2022-02-28 03:33:21 -0500

    Updated LFO definitions

--- src/inc/Lfo.h
+++ src/inc/Lfo.h
@@ -11,32 +11,57 @@
 
 class CLfo {
 public:
+    CLfo();
+
+    virtual ~CLfo();
 
     enum Param_t{
         kAmplitude,
         kFrequency,
     };
 
-    Error_t init (float fSampleRateInHz, int iTableSize);
+    Error_t init (float fSampleRateInHz, int iTableSize){
+        m_fSampleRate = fSampleRateInHz;
+        m_iTableSize = iTableSize;
+        m_fCurrentIndex = 0.f;
+        m_amplitude = 0.f;
+        m_frequency = 0.f;
+        m_pCRingBuffer = new CRingBuffer<float>(m_iTableSize);
+        return Error_t::kNoError;
+    }
 
-    Error_t reset ();
+    Error_t reset (){
+    };
 
-    Error_t setParam (Param_t eParam, float fParamValue){};
+    Error_t setParam (Param_t eParam, float fParamValue) {
+        switch (eParam) {
+            case Param_t::kAmplitude:
+                m_amplitude = fParamValue;
+                break;
+            case Param_t::kFrequency:
+                m_frequency = fParamValue;
+                break;
+        }
+        return Error_t::kNoError;
+    };
 
-    float getParam (Param_t eParam) const;
+    float getParam (Param_t eParam) const {
+        switch (eParam) {
+            case Param_t::kAmplitude:
+                return m_amplitude;
+            case Param_t::kFrequency:
+                return m_frequency;
+        }
+        return -1.f;
+    }
 
     float process();
 
-
-protected:
-
-    CLfo();
-
-    virtual ~CLfo();
-
 private:
 
-    float           m_fSampleRate, m_fCurrentIndex;
+    float m_fSampleRate, m_fCurrentIndex, m_amplitude, m_frequency;
+
+    int m_iTableSize;
 
     CRingBuffer<float> *m_pCRingBuffer;
 
