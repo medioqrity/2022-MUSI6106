8b576b6b49773ab6df8201a041db29e20ce5198b
medioqrity <medioqrity@gmail.com>
2022-04-17 17:58:14 -0400

    add identity test for FFT

--- src/Tests/Tests/Test_FastConv.cpp
+++ src/Tests/Tests/Test_FastConv.cpp
@@ -19,7 +19,7 @@ namespace fastconv_test {
         }
     }
 
-    class FastConv: public testing::Test
+    class TimeDomainConv: public testing::Test
     {
     protected:
         void SetUp() override
@@ -52,7 +52,7 @@ namespace fastconv_test {
         CFastConv *m_pCFastConv = nullptr;
     };
 
-    TEST_F(FastConv, identity)
+    TEST_F(TimeDomainConv, identity)
     {
         // initialize input signal, which is $\delta[n-3]$
         int shift = 3;
@@ -81,7 +81,7 @@ namespace fastconv_test {
         delete[] output;
     }
 
-    TEST_F(FastConv, flushbuffer) {
+    TEST_F(TimeDomainConv, flushbuffer) {
         // initialize input signal, which is $\delta[n-3]$
         int shift = 3;
         int signalLength = 10;
@@ -109,7 +109,7 @@ namespace fastconv_test {
         delete[] output;
     }
 
-    TEST_F(FastConv, varyingBlockSize) {
+    TEST_F(TimeDomainConv, varyingBlockSize) {
         // initialize input signal, which is $\delta[n-3]$
         int shift = 3;
         int signalLength = 10000;
@@ -142,6 +142,68 @@ namespace fastconv_test {
         // also check if tails are all zero
         for (int i = IRLength + shift; i < signalLength; ++i) {
             EXPECT_EQ(output[i], 0);
+        }
+
+        delete[] input;
+        delete[] output;
+    }
+
+    class FFTFastConv: public testing::Test
+    {
+    protected:
+        void SetUp() override
+        {
+            m_pCFastConv = new CFastConv();
+
+            // generate a random IR of length 51 samples
+            IRLength = 51;
+            IR = new float[IRLength];
+            memset(IR, 0, sizeof(float) * IRLength);
+
+            // use mt19937 & distribution for high quality random number generation
+            std::mt19937 generator(114514); // 114514 is seed
+            std::uniform_real_distribution<float> uniformRealDistribution(-1.F, 1.F);
+            for (int i = 0; i < IRLength; ++i) {
+                IR[i] = uniformRealDistribution(generator);
+            }
+
+            m_pCFastConv->init(IR, IRLength, 32, CFastConv::kFreqDomain);
+        }
+
+        virtual void TearDown()
+        {
+            m_pCFastConv->reset();
+            delete[] IR;
+        }
+
+        int IRLength = 0;
+        float* IR = nullptr;
+        CFastConv *m_pCFastConv = nullptr;
+    };
+
+    TEST_F(FFTFastConv, identity)
+    {
+        // initialize input signal, which is $\delta[n-3]$
+        int shift = 3;
+        int signalLength = 10;
+        float* input = new float[signalLength]; // make the input signal 10 samples long
+        memset(input, 0, sizeof(float) * signalLength); // an impulse as input signal at sample index 3
+        input[shift] = 1.F;
+
+        // initialize output signal
+        float* output = new float[signalLength];
+        memset(output, 0, sizeof(float) * signalLength);
+
+        m_pCFastConv->process(output, input, signalLength);
+
+        // check if the output signal is exactly the same with the impulse response
+        for (int i = 0; i + shift < signalLength; ++i) {
+            EXPECT_NEAR(IR[i], output[i + shift], 1e-6);
+        }
+
+        // also check if the first three are ok
+        for (int i = 0; i < shift; ++i) {
+            EXPECT_NEAR(output[i], 0, 1e-6);
         }
 
         delete[] input;
