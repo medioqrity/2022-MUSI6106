95304ecc833c48f2f506afc968ddf77fa9f00681
medioqrity <medioqrity@gmail.com>
2022-04-25 16:24:00 -0400

    switch to c++17 (https://github.com/medioqrity/2022-MUSI6106/pull/18#discussion_r856610540); rewrite exceptions in MUSI6106Exec.cpp (https://github.com/medioqrity/2022-MUSI6106/pull/18#discussion_r856620682)

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -129,7 +129,7 @@ public:
         CAudioFileIf::create(phAudioFile);
 
         if (phAudioFile == nullptr) {
-            // throw std::exception("failed to create audio file handler");
+            throw std::runtime_error("failed to create audio file handler");
         }
 
         switch (ioType)
@@ -142,16 +142,16 @@ public:
         case CAudioFileIf::kFileWrite:
             phAudioFile->openFile(filepath, ioType, fileSpec);
             break;
-        // default:
-        //     throw std::exception("wrong parameter of ioType (should be read / write)");
+        default:
+            throw std::invalid_argument("wrong parameter of ioType (should be read / write)");
         }
 
         if (!phAudioFile->isOpen()) {
-            // throw std::exception("failed to open file");
+            throw std::runtime_error("failed to open file");
         }
 
         if (this->fileSpec == nullptr) {
-            // throw std::exception("failed to load file spec");
+            throw std::runtime_error("failed to load file spec");
         }
 
         phAudioFile->getLength(fileNumSample);
@@ -211,12 +211,12 @@ public:
     }
 
     bool isEof() const{
-        // if (phAudioFile == nullptr) throw new std::exception("audio file handler has not been initialized");
+        if (phAudioFile == nullptr) throw new std::logic_error("audio file handler has not been initialized");
         return phAudioFile->isEof();
     }
 
     int getNumChannels() const {
-        // if (fileSpec == nullptr) throw new std::exception("file spec is nullptr, cannot get channel count");
+        if (fileSpec == nullptr) throw new std::logic_error("file spec is nullptr, cannot get channel count");
         return fileSpec->iNumChannels;
     }
 
